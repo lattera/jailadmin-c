07e90063636599db2b4cd4557bfcca68e04dcd8a
lattera <lattera@gmail.com>
2012-03-24 09:35:59 -0600

    Fix tabs

--- src/list.c
+++ src/list.c
@@ -8,37 +8,37 @@
 
 LISTNODE *AddNode(LINKEDLIST *list, char *name, void *data, unsigned long sz)
 {
-	LISTNODE *node;
+    LISTNODE *node;
 
-	if (!(list))
-		return NULL;
+    if (!(list))
+        return NULL;
 
-	if (!(list->head)) {/*uninitialized list! set up head & tail nodes.*/
-		list->head = malloc(sizeof(LISTNODE));
-		if (!(list->head))
-			return NULL;
-		list->tail = list->head; /*initial degenerate case, head==tail*/
-		node = list->tail;
-	} else {
-		list->tail->next = malloc(sizeof(LISTNODE));/*make room for the next one*/
-		if (!(list->tail->next))
-			return NULL;
-		list->tail = list->tail->next; /*tail must always point to the last node*/
-		node = list->tail; /*tail is always the last node by definition*/
-	}
-	memset(node, 0x00, sizeof(LISTNODE));
+    if (!(list->head)) {/*uninitialized list! set up head & tail nodes.*/
+        list->head = malloc(sizeof(LISTNODE));
+        if (!(list->head))
+            return NULL;
+        list->tail = list->head; /*initial degenerate case, head==tail*/
+        node = list->tail;
+    } else {
+        list->tail->next = malloc(sizeof(LISTNODE));/*make room for the next one*/
+        if (!(list->tail->next))
+            return NULL;
+        list->tail = list->tail->next; /*tail must always point to the last node*/
+        node = list->tail; /*tail is always the last node by definition*/
+    }
+    memset(node, 0x00, sizeof(LISTNODE));
 
     /* Yeah, small memory leak here */
     if ((name))
         if (!(node->name = strdup(name)))
             return NULL;
     
-	node->data = malloc(sz);
-	memset(node->data, 0x00, sz);
-	node->sz = sz;
-	memcpy(node->data, data, sz);
+    node->data = malloc(sz);
+    memset(node->data, 0x00, sz);
+    node->sz = sz;
+    memcpy(node->data, data, sz);
 
-	return node;
+    return node;
 }
 
 void _free_node(LISTNODE *node)
@@ -54,68 +54,68 @@ void _free_node(LISTNODE *node)
 
 void DeleteNode(LINKEDLIST *list, LISTNODE *node)
 {
-	LISTNODE *del;
+    LISTNODE *del;
 
-	if (!(list))
-		return;
+    if (!(list))
+        return;
 
-	if (node == list->head) {
-		list->head = node->next;
+    if (node == list->head) {
+        list->head = node->next;
 
         _free_node(node);
 
-		return;
-	}
+        return;
+    }
 
-	del = list->head;
-	while (del != NULL && del->next != node)
-		del = del->next;
+    del = list->head;
+    while (del != NULL && del->next != node)
+        del = del->next;
 
-	if (!(del))
-		return;
-	
-	if(del->next == list->tail)
-		list->tail = del;
+    if (!(del))
+        return;
+    
+    if(del->next == list->tail)
+        list->tail = del;
 
-	del->next = del->next->next;
-	
+    del->next = del->next->next;
+    
     _free_node(node);
 }
 
 LISTNODE *FindNodeByRef(LINKEDLIST *list, void *data)
 {
-	LISTNODE *node;
+    LISTNODE *node;
 
-	if (!(list))
-		return NULL;
+    if (!(list))
+        return NULL;
 
-	node = list->head;
-	while ((node)) {
-		if (node->data == data)
-			return node;
+    node = list->head;
+    while ((node)) {
+        if (node->data == data)
+            return node;
 
-		node = node->next;
-	}
+        node = node->next;
+    }
 
-	return NULL;
+    return NULL;
 }
 
 LISTNODE *FindNodeByValue(LINKEDLIST *list, void *data, unsigned long sz)
 {
-	LISTNODE *node;
+    LISTNODE *node;
 
-	if (!(list))
-		return NULL;
+    if (!(list))
+        return NULL;
 
-	node = list->head;
-	while ((node)) {
-		if (!memcmp(node->data, data, (sz > node->sz) ? node->sz : sz))
-			return node;
+    node = list->head;
+    while ((node)) {
+        if (!memcmp(node->data, data, (sz > node->sz) ? node->sz : sz))
+            return node;
 
-		node = node->next;
-	}
+        node = node->next;
+    }
 
-	return NULL;
+    return NULL;
 }
 
 LISTNODE *FindNodeByName(LINKEDLIST *list, char *name)
@@ -140,20 +140,20 @@ LISTNODE *FindNodeByName(LINKEDLIST *list, char *name)
 /*frees all nodes in a list, including their data*/
 void FreeNodes(LINKEDLIST *list, bool free_list)
 {
-	if (!(list))
-		return;
+    if (!(list))
+        return;
 
-	while ((list->head))
+    while ((list->head))
         DeleteNode(list, list->head);
 
-	if (free_list)
+    if (free_list)
         free(list);
-	
-	return;
+    
+    return;
 }
 
 void FreeList(LINKEDLIST *list)
 {
-	FreeNodes (list, true);
-	return;
+    FreeNodes (list, true);
+    return;
 }
