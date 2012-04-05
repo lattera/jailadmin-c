490998ea752cd67b92ebc94cdc102cb62d49bf75
Shawn Webb <lattera@gmail.com>
2012-04-05 15:33:07 -0600

    Show the status of the jails

--- src/interactive.c
+++ src/interactive.c
@@ -27,11 +27,34 @@ void interactive(JAILADMIN *admin)
 
     initscr();
 
+    if (has_colors()) {
+        start_color();
+        init_pair(1, COLOR_RED, COLOR_BLACK);
+        init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    }
+
     jails = get_jails(admin);
     while (1) {
         move(0,0);
         for (i=0; jails[i] != NULL; i++) {
-            printw("[%s] %s\n", jails[i]->name, (is_jail_fully_online(jails[i])) ? "true" : "false");
+            if (has_colors()) {
+                printw("[");
+                if (is_jail_fully_online(jails[i]))
+                    attron(COLOR_PAIR(2));
+                else
+                    attron(COLOR_PAIR(1));
+
+                printw("%s", jails[i]->name);
+
+                if (is_jail_fully_online(jails[i]))
+                    attroff(COLOR_PAIR(2));
+                else
+                    attroff(COLOR_PAIR(1));
+                printw("]\n");
+            } else {
+                printw("[%s] %s\n", jails[i]->name, (is_jail_fully_online(jails[i])) ? "online" : "offline");
+            }
+
             refresh();
         }
 
