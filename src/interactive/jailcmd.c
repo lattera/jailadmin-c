dc7c2d1b8b5b35478773a7f232211a30187b17de
lattera <lattera@gmail.com>
2012-04-07 11:39:23 -0600

    Fix bugs. Add ability to stop jails.

--- src/interactive/jailcmd.c
+++ src/interactive/jailcmd.c
@@ -10,9 +10,6 @@ CALLBACK_RETURN jail_cmd(JAILADMIN *admin, char **cmd)
     if (strcasecmp(cmd[0], "jail"))
         return CONT_PROC;
 
-    wprintw(admin->windows[MAINWIN], "You're trying to use the jail cmd\n");
-    wrefresh(admin->windows[MAINWIN]);
-
     optreset = optind = 1;
     numargs = num_args(cmd);
     while ((ch = getopt(numargs, cmd, "s:S:c:")) != -1) {
@@ -21,21 +18,31 @@ CALLBACK_RETURN jail_cmd(JAILADMIN *admin, char **cmd)
                 jail = get_jail(admin, optarg);
                 if ((jail)) {
                     if (start_jail(admin, jail) == jatrue)
-                        wprintw(admin->windows[MAINWIN], "Jail %s started successfully\n");
+                        wprintw(admin->windows[MAINWIN], "Jail %s started successfully\n", optarg);
                     else
-                        wprintw(admin->windows[MAINWIN], "Jail %s failed to start\n");
+                        wprintw(admin->windows[MAINWIN], "Jail %s failed to start\n", optarg);
                 }
                 else
                     wprintw(admin->windows[MAINWIN], "No such jail: %s\n", optarg);
                 break;
             case 'S':
-                /* stop jail */
+                jail = get_jail(admin, optarg);
+                if ((jail)) {
+                    if (stop_jail(admin, jail) == jatrue)
+                        wprintw(admin->windows[MAINWIN], "Jail %s stopped successfully\n", optarg);
+                    else
+                        wprintw(admin->windows[MAINWIN], "Jail %s failed to stop\n", optarg);
+                }
+                else
+                    wprintw(admin->windows[MAINWIN], "No such jail: %s\n", optarg);
                 break;
             case 'c':
                 /* config jail */
                 break;
         }
     }
+
+    wrefresh(admin->windows[MAINWIN]);
 
     return CONT_PROC;
 }
